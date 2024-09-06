1fdbbabaea7b405ce43bc6d514c9d03073d0c50a
acs027 <alicihansarac@gmail.com>
2024-09-07 00:59:24 +0300

    Initial Commit

---
+++ InstaProfilePage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaProfilePage
+//
+//  Created by ali cihan on 7.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
